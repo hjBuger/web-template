3b3607efb5a2a3fc5b59d2f54bf41bb5b9f0fba9
huangjinbao <huangjinbao@idea-group.cn>
2022-05-17 15:56:24 +0800

    1

--- src/pages/demo/test1.vue
+++ src/pages/demo/test1.vue
@@ -21,10 +21,10 @@
                     <el-button>点击</el-button>
                 </template>
                 <template v-slot="props">
-                    <div style="color:red;">{{props.item.label}}</div>
-                    <div style="color:red;">{{props.data.name}}</div>
-                    <div style="color:red;">{{props.$index}}</div>
-                    测试新的组件标题测试新的组件标题测试新的组件标题测试新的组件标题
+                    <div style="color:red;">label：{{props.item.label}}</div>
+                    <div style="color:blue;">原始数据名称：{{props.data.name}}</div>
+                    <div style="color:green;margin-bottom: 10px;">序号：{{props.$index}}</div>
+                    内容插槽测试...
                 </template>
             </sapi-simple-mark>
         </div>
@@ -35,56 +35,33 @@
         menuLabel: '测试1',
         data () {
             return {
-                markData: [
-                    {
-                        label: '拿地',
-                        // labelStyle: {
-                        //     fontWeight: 'bold',
-                        //     fontSize: '16px'
-                        // },
-                        value: '11',
-                        status: '1',
-                        content: `2022-05-10`,
-                        clickable: true,
-                        data: {
-                            name: 'hhhhhhhhhhhhhh'
-                        },
-                        clickFn: (item) => {
-                            console.log('item: ', item)
+                markData: [],
+                // 状态匹配（对象）
+                statusMap: {
+                    1: {
+                        label: '未开始',
+                        color: 'gray',
+                        icon: 'el-icon-monitor'
+                    },
+                    ' 2 , 5 ': {
+                        label: '进行中',
+                        color: 'red',
+                        icon: {
+                            type: 'text',
+                            content: '进'
                         }
                     },
-                    {
-                        label: '项目立项会',
-                        value: '22',
-                        status: '2',
-                        content: '22222'
-                    },
-                    {
-                        label: '项目预备会',
-                        value: '33',
-                        status: '3',
-                        content: '33333'
-                    },
-                    {
-                        label: '项目启动会',
-                        value: '44',
-                        status: '2',
-                        content: '44444'
-                    },
-                    {
-                        label: '总包开工',
-                        value: '55',
-                        status: '5',
-                        content: '55555'
-                    },
-                    {
-                        label: '总包开工2',
-                        value: '66',
-                        status: '4',
-                        content: '66666'
-                    },
-                ],
-                options: [
+                    '3 , 4': {
+                        label: '已完成',
+                        color: 'green',
+                        icon: {
+                            type: 'img',
+                            content: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdingyue.ws.126.net%2F2020%2F0515%2F465567a6j00qadpfz001cc000hs00b4c.jpg&refer=http%3A%2F%2Fdingyue.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654769533&t=64e379f7a431131b5bcd1e1571d19213'
+                        }
+                    }
+                },
+                // 状态匹配（数组）
+                statusMap2: [
                     {
                         label: '未开始',
                         value: '1',
@@ -115,31 +92,34 @@
                         },
                         lineType: 'solid'
                     }
-                ],
-                statusMap: {
-                    1: {
-                        value: '1',
-                        color: 'gray',
-                        icon: 'el-icon-monitor'
-                    },
-                    ' 2 , 5 ': {
-                        value: ['2', '5'],
-                        color: 'red',
-                        icon: {
-                            type: 'text',
-                            content: '进'
-                        }
-                    },
-                    '3 , 4': {
-                        value: '3,4',
-                        color: 'green',
-                        icon: {
-                            type: 'img',
-                            content: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdingyue.ws.126.net%2F2020%2F0515%2F465567a6j00qadpfz001cc000hs00b4c.jpg&refer=http%3A%2F%2Fdingyue.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654769533&t=64e379f7a431131b5bcd1e1571d19213'
-                        }
-                    }
-                }
+                ]
             }
+        },
+        methods: {
+            loadData () {
+                // 数据源
+                const demo = [
+                    {id: '1', name: '拿地', status: 1, date: '2022-05-10'},
+                    {id: '2', name: '项目立项会', status: 2, date: '2022-05-10'},
+                    {id: '3', name: '项目预备会', status: 3, date: '2022-05-10'},
+                    {id: '4', name: '项目启动会', status: 4, date: '2022-05-10'},
+                    {id: '5', name: '总包开工', status: 5, date: '2022-05-10'},
+                    {id: '6', name: '总包开工2', status: 3, date: '2022-05-10'}
+                ]
+
+                // 根据业务构造数据
+                const markData = demo.map(item => ({
+                    data: item,
+                    label: item.name,
+                    value: item.id,
+                    status: item.status,
+                    content: item.date
+                }))
+                this.markData = markData
+            }
+        },
+        mounted () {
+            this.loadData()
         }
     }
 </script>
